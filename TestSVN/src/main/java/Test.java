23dd74caa3980dac655dbae18247c0bde947d7a3
bighome@te.net.ua <bighome@te.net.ua@6957ccf0-b8fa-a08f-3310-73e8f4dc9c2b>
2012-12-09 17:04:19 +0000

    Test of using SVN.

--- TestSVN/src/main/java/Test.java
+++ TestSVN/src/main/java/Test.java
@@ -2,5 +2,6 @@
 public class Test {
     public static void main(String args[ ]){
         System.out.println("Hello My Name is Chirko Lorans");
+        System.out.println("Hello! My name is Belov Alexandr!");
     }
 }

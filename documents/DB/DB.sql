4fda5315ea5489b50a44bcc7d2912fd5408ec110
NffS92@gmail.com <NffS92@gmail.com@6957ccf0-b8fa-a08f-3310-73e8f4dc9c2b>
2013-01-05 09:20:50 +0000

    nc-19 Написание скриптов на создание таблиц "User" и "User_type"

---
+++ documents/DB/DB.sql
@@ -0,0 +1,21 @@
+DROP TABLE Users     	CASCADE CONSTRAINTS;
+DROP TABLE Users_type   CASCADE CONSTRAINTS;
+
+CREATE TABLE Users_type
+    ( 
+		user_type_id    NUMBER(6) NOT NULL PRIMARY KEY,
+		type_name    	VARCHAR2(20) NOT NULL
+    );
+
+CREATE TABLE Users
+    ( 
+		user_id    		NUMBER(6) NOT NULL PRIMARY KEY,
+		email   		VARCHAR2(20) NOT NULL,
+		password 		VARCHAR2(16) NOT NULL,
+		first_name  	VARCHAR2(20) NOT NULL,
+		surname			VARCHAR2(20) NOT NULL,
+		last_name		VARCHAR2(20) NOT NULL,
+		foto			VARCHAR2(40),
+		user_type_id	NUMBER(6) REFERENCES Users_type (user_type_id)
+    );
+

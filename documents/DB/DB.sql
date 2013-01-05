9519418d34f8caaf713a1036af0803e2ea002b32
bighome@te.net.ua <bighome@te.net.ua@6957ccf0-b8fa-a08f-3310-73e8f4dc9c2b>
2013-01-05 10:38:24 +0000

    nc-30 Создание таблиц "HR_mark", "Tech_mark" и "Interview"

--- documents/DB/DB.sql
+++ documents/DB/DB.sql
@@ -1,5 +1,8 @@
 DROP TABLE Users     	CASCADE CONSTRAINTS;
 DROP TABLE Users_type   CASCADE CONSTRAINTS;
+DROP TABLE HR_mark;
+DROP TABLE Tech_mark;
+DROP TABLE Interview;
 
 CREATE TABLE Users_type
     ( 
@@ -19,5 +22,35 @@ CREATE TABLE Users
 		user_type_id	NUMBER(6) REFERENCES Users_type (user_type_id)
     );
 
+CREATE TABLE HR_mark
+	(
+		hr_mark_id 		NUMBER(6) NOT NULL PRIMARY KEY,
+		hr_id			NUMBER(6) NOT NULL REFERENCES Users (user_id),
+		form_id			NUMBER(6) NOT NULL REFERENCES Form (form_id),
+		motivation 		NUMBER(3) NOT NULL,
+		english			NUMBER(3) NOT NULL
+	);
+	
+CREATE TABLE Tech_mark
+	(
+		tech_mark_id 	NUMBER(6) NOT NULL PRIMARY KEY,
+		interviewer_id	NUMBER(6) NOT NULL REFERENCES Users (user_id),
+		form_id			NUMBER(6) NOT NULL REFERENCES Form (form_id),
+		prog_lang 		NUMBER(3) NOT NULL,
+		oop				NUMBER(3) NOT NULL,
+		patterns		NUMBER(3) NOT NULL,
+		db				NUMBER(3) NOT NULL,
+		cs				NUMBER(3) NOT NULL,
+		exp				NUMBER(3) NOT NULL,
+		other			NUMBER(3) NOT NULL
+	);
 
+CREATE TABLE Interview
+	(
+		interview_id	NUMBER(6) NOT NULL PRIMARY KEY,
+		start_date		DATE NOT NULL,
+		end_date		DATE NOT NULL,
+		seats	 		NUMBER(3) NOT NULL
+	);
+	
 COMMIT;

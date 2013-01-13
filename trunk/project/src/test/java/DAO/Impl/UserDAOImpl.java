7a3c99eb8132ef1d0da24778e088364c60cced7c
bighome@te.net.ua <bighome@te.net.ua@6957ccf0-b8fa-a08f-3310-73e8f4dc9c2b>
2013-01-13 14:54:45 +0000

    Папка с заготовкой проекта. Импортировать в eclipse в качестве maven-проекта.

---
+++ trunk/project/src/test/java/DAO/Impl/UserDAOImpl.java
@@ -0,0 +1,93 @@
+package DAO.Impl;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.JOptionPane;
+import org.hibernate.Session;
+import util.HibernateUtil;
+import DAO.UserDAO;
+import test.User;
+
+public class UserDAOImpl implements UserDAO {
+   
+    public void addUser(User usr) throws SQLException {
+            Session session = null;
+            try {
+                session = HibernateUtil.getSessionFactory().openSession();
+                session.beginTransaction();
+                session.save(usr);
+                session.getTransaction().commit();
+            } catch (Exception e) {
+                JOptionPane.showMessageDialog(null, e.getMessage(), "Ошибка I/O", JOptionPane.OK_OPTION);
+            } finally {
+                if (session != null && session.isOpen()) {
+                    session.close();
+                }
+            }
+      }
+
+      public void updateUser(User usr) throws SQLException {
+            Session session = null;
+            try {
+                session = HibernateUtil.getSessionFactory().openSession();
+                session.beginTransaction();
+                session.update(usr);
+                session.getTransaction().commit();
+            } catch (Exception e) {
+                JOptionPane.showMessageDialog(null, e.getMessage(), "Ошибка I/O", JOptionPane.OK_OPTION);
+            } finally {
+                if (session != null && session.isOpen()) {
+                    session.close();
+                }
+            }
+      }
+
+      public User getUserById(int id) throws SQLException {
+            Session session = null;
+            User usr = null;
+            try {
+                session = HibernateUtil.getSessionFactory().openSession();
+                usr = (User) session.load(User.class, id);
+            } catch (Exception e) {
+                JOptionPane.showMessageDialog(null, e.getMessage(), "Ошибка I/O", JOptionPane.OK_OPTION);
+            } finally {
+                if (session != null && session.isOpen()) {
+                    session.close();
+                }
+            }
+            return usr;
+      }
+
+      public List<User> getAllUsers() throws SQLException {
+            Session session = null;
+            List<User> usrs = new ArrayList<User>();
+            try {
+                session = HibernateUtil.getSessionFactory().openSession();
+                usrs = session.createCriteria(User.class).list();
+            } catch (Exception e) {
+                JOptionPane.showMessageDialog(null, e.getMessage(), "Ошибка I/O", JOptionPane.OK_OPTION);
+            } finally {
+                if (session != null && session.isOpen()) {
+                    session.close();
+                }
+            }
+            return usrs;
+      }
+
+      public void deleteUser(User usr) throws SQLException {
+            Session session = null;
+            try {
+                session = HibernateUtil.getSessionFactory().openSession();
+                session.beginTransaction();
+                session.delete(usr);
+                session.getTransaction().commit();
+            } catch (Exception e) {
+                JOptionPane.showMessageDialog(null, e.getMessage(), "Ошибка I/O", JOptionPane.OK_OPTION);
+            } finally {
+                if (session != null && session.isOpen()) {
+                    session.close();
+                }
+            }
+      }  
+}

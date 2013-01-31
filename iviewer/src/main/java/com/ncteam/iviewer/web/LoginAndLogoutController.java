c5565730cfddb47ec43229778828562a9c61f79a
bighome@te.net.ua <bighome@te.net.ua@6957ccf0-b8fa-a08f-3310-73e8f4dc9c2b>
2013-01-31 15:42:25 +0000

    Update: Добавлен маппинг таблицы Tech_mark, который в прошлой версии не был включён в список файлов. Произведены незначительные изменения при входе пользователя.

---
+++ iviewer/src/main/java/com/ncteam/iviewer/web/LoginAndLogoutController.java
@@ -0,0 +1,75 @@
+package com.ncteam.iviewer.web;
+
+import java.util.Map;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import com.ncteam.iviewer.domain.User;
+import com.ncteam.iviewer.service.UserServiceImpl;
+
+@Controller
+public class LoginAndLogoutController {
+
+	
+	private UserServiceImpl userService=new UserServiceImpl();
+	
+	@RequestMapping(value = "/loginCheck", method = RequestMethod.POST)
+	public String checkUserLogin(@ModelAttribute("usr") User user, Map<String, Object> map,
+			HttpSession session){
+		
+		String message="";
+		String email=user.getEmail();
+		String password=user.getPassword();
+		
+		if(!(email.isEmpty())&&!(password.isEmpty())){
+			
+			User resultUser=userService.getUserByEmail(user.getEmail());
+			
+			if(resultUser!=null){
+				if(resultUser.getPassword().equals(password)){
+					session.setAttribute("user", resultUser);
+					session.setAttribute("email", resultUser.getEmail());
+					session.setAttribute("first_name", resultUser.getFirst_name());
+					session.setAttribute("surname", resultUser.getSurname());
+					session.setAttribute("foto", resultUser.getFoto());
+					session.setAttribute("user_type_id", resultUser.getUser_type_id());
+					return "redirect:/index";
+				}
+			else{
+					message="Введённый неверный пароль.";
+				}
+			}
+			else{
+				message="Пользователя с таким email'ом не существует.";
+			}
+		}
+		else{
+			message="Поля \"Email\" и \"Пароль\" должны быть заполнены!";
+		}
+		map.put("email", email);
+		map.put("password", password);
+		map.put("message", message);
+		return "index";
+	}
+		
+	@RequestMapping("/logout")
+	public String logout(HttpSession session){
+		session.removeAttribute("user");
+		session.removeAttribute("email");
+		session.removeAttribute("first_name");
+		session.removeAttribute("surname");
+		session.removeAttribute("foto");
+		session.removeAttribute("user_type_id");
+		return "redirect:/index";
+	}
+	
+	@ModelAttribute("usr")
+	public User addUser(){
+		return new User();
+	}
+
+}

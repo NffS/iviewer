c3217298c7742ef3de9a0a24aab0d507856a01bd
NffS92@gmail.com <NffS92@gmail.com@6957ccf0-b8fa-a08f-3310-73e8f4dc9c2b>
2013-02-02 13:08:22 +0000

    Добавил вывод список пользователей админу и шаблон для редактирования пользовтелей.

--- iviewer/src/main/java/com/ncteam/iviewer/web/AdminController.java
+++ iviewer/src/main/java/com/ncteam/iviewer/web/AdminController.java
@@ -1,10 +1,14 @@
 package com.ncteam.iviewer.web;
 
+import com.ncteam.iviewer.domain.User;
+import com.ncteam.iviewer.domain.Users_type;
 import com.ncteam.iviewer.service.TablesService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -24,9 +28,22 @@ public class AdminController {
     public String showAdmin(Map<String, Object> map) {
 
 
+        List<User> users=tablesService.getAllRecords(User.class);
+        List<Users_type> users_types=tablesService.getAllRecords(Users_type.class);
 
-        //map.put("newsText", tablesService.getRecordById(4,News.class).getText());
+        map.put("users_type", users_types);
+        map.put("users", users);
 
         return "admin";
+    }
+
+    @RequestMapping("/user_edit")
+    public String userEdit(Map<String, Object> map) {
+        return "user_edit";
+    }
+
+    @ModelAttribute("edit_user")
+    public User addUser(){
+        return new User();
     }
 }
--- iviewer/src/main/webapp/WEB-INF/views/admin.jsp
+++ iviewer/src/main/webapp/WEB-INF/views/admin.jsp
@@ -1,5 +1,7 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
+<%@taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+
 <%@include file="/resources/design/header.jsp" %>
 
 <button type="button" class="btn btn-primary" data-toggle="button">Редактировать</button>
@@ -8,8 +10,20 @@
 <li><a href="#">что то еще</a></li>
 
 <%@ include file="/resources/design/center.jsp" %>
+<%int k=0; %>
+<table width="100%" border=2 align="center">
+    <tr ALIGN=center bgcolor="9999FF">
+        <th>№</th> <th>ФИО</th> <th>Тип</th> <th>Действие</th>
+    </tr>
+    <c:forEach var="users" items="${users}">
+        <%k++; %>
+        <tr ALIGN=center height=40>
+            <td><%=k %></td> <td width=300>${users.surname}  ${users.first_name} ${users.last_name}</td>
+            <td>${users.user_type_id}</td> <td><a href="/user_edit">Редактировать</a></td>
 
-Вы вошли как админ.
+        </tr>
+    </c:forEach>
+</table>
 
 <%@ include file="/resources/design/footer.jsp" %>
 

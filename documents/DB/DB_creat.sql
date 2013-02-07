d65705849c0d07540a10436e475ec8adcba2563b
NffS92@gmail.com <NffS92@gmail.com@6957ccf0-b8fa-a08f-3310-73e8f4dc9c2b>
2013-02-07 16:41:04 +0000

    Изменена БД!!. Добавлена таблица Form_backup, в Users добавлено поле Confirm_status

--- documents/DB/DB_creat.sql
+++ documents/DB/DB_creat.sql
@@ -5,6 +5,8 @@ DROP TABLE Tech_mark	CASCADE CONSTRAINTS;
 DROP TABLE Interview	CASCADE CONSTRAINTS;
 DROP TABLE Form			CASCADE CONSTRAINTS;
 DROP TABLE News			CASCADE CONSTRAINTS;
+DROP TABLE Form_backup	CASCADE CONSTRAINTS;
+
 
 CREATE TABLE Users_type
     ( 
@@ -39,7 +41,7 @@ CREATE TABLE Interview
 CREATE TABLE Form
 	(
 		form_id		 		NUMBER(6) NOT NULL PRIMARY KEY,
-		user_id		NUMBER(6) NOT NULL REFERENCES Users (user_id),
+		user_id				NUMBER(6) NOT NULL REFERENCES Users (user_id),
 		university			VARCHAR2(400) CONSTRAINT university
 		CHECK (REGEXP_LIKE(university,'[a-zA-Zà-ÿÀ-ß0-9]$')),
 		faculty 			VARCHAR2(400) CONSTRAINT faculty
@@ -102,7 +104,48 @@ CREATE TABLE Form
 		interview_id		NUMBER(6) REFERENCES Interview (interview_id),
 		visit_status		NUMBER(1)
 	);
-
+	
+	
+CREATE TABLE Form_backup
+	(
+		form_bakup_id		NUMBER(6) NOT NULL PRIMARY KEY,
+		form_id		 		NUMBER(6) NOT NULL REFERENCES Form (form_id),
+		university			VARCHAR2(400),
+		faculty 			VARCHAR2(400),
+		course	 			NUMBER(1),
+		end_year			VARCHAR2(5),
+		email2				VARCHAR2(30),
+		phone				VARCHAR2(13),
+		another_contact		VARCHAR2(400),
+		interest_tc 		VARCHAR2(2) NOT NULL,
+		interest_nc 		VARCHAR2(2) NOT NULL,
+		interest_area_po 	VARCHAR2(2)	NOT NULL,
+		interest_area_other VARCHAR2(800),
+		job_ar_deep_spec 	VARCHAR2(2) NOT NULL,
+		job_ar_varied 		VARCHAR2(2) NOT NULL,
+		job_ar_manage 		VARCHAR2(2) NOT NULL,
+		job_ar_sales		VARCHAR2(2) NOT NULL,
+		job_ar_other 		VARCHAR2(800),
+		prog_lang_c 		NUMBER(1) NOT NULL,
+		prog_lang_java		NUMBER(1) NOT NULL,
+		prog_lang_other 	VARCHAR2(800),
+		cs_network_tech 	NUMBER(1) NOT NULL,
+		cs_algorithms 		NUMBER(1) NOT NULL,
+		cs_oop	 			NUMBER(1) NOT NULL,
+		cs_gui 				NUMBER(1) NOT NULL,
+		cs_db 				NUMBER(1) NOT NULL,
+		cs_web 				NUMBER(1) NOT NULL,
+		cs_network_prog 	NUMBER(1) NOT NULL,
+		cs_design 			NUMBER(1) NOT NULL,
+		experience			VARCHAR2(4000),
+		english_read 		NUMBER(1) NOT NULL,
+		english_write		NUMBER(1) NOT NULL,
+		english_spoken	 	NUMBER(1) NOT NULL,
+		source				VARCHAR2(800) NOT NULL,
+		motivation_comment	VARCHAR2(2000) NOT NULL,
+		comment2			VARCHAR2(2000) NOT NULL		
+	);
+	
 CREATE TABLE HR_mark
 	(
 		hr_mark_id 		NUMBER(6) NOT NULL PRIMARY KEY,

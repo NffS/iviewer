3ba73bb59115bd110be3e5d9f7261a294d1f29a0
bighome@te.net.ua <bighome@te.net.ua@6957ccf0-b8fa-a08f-3310-73e8f4dc9c2b>
2013-02-08 18:57:12 +0000

    Добавлена дополнительная проверка правильности введения собеседований в контроллере.

--- trunk/iviewer/src/main/java/com/ncteam/iviewer/web/HRController.java
+++ trunk/iviewer/src/main/java/com/ncteam/iviewer/web/HRController.java
@@ -33,7 +33,7 @@ public class HRController {
 	public String redactInterviewFromHRPage(HttpServletRequest request, HttpSession session,
 			@PathVariable("interview_id") Integer interview_id,	Map<String, Object> map){
 		
-		if(((Integer)session.getAttribute("user_type")).equals(2)){
+		if(((Integer)session.getAttribute("user_type_id")).equals(2)){
 		
 		String updateStartDateString=((String)request.getParameter("date"))
 				.concat(" "+(String)request.getParameter("startTime"));
@@ -41,6 +41,11 @@ public class HRController {
 		String updateEndDateString=((String)request.getParameter("date"))
 				.concat(" "+(String)request.getParameter("endTime"));
 		
+		if(isInterviewDateStringValid(updateStartDateString)
+				&&isInterviewDateStringValid(updateEndDateString)){
+			
+			if(isInterviewStartEarlierThanEnd((String)request.getParameter("startTime"),
+					(String)request.getParameter("endTime"))){
 		Interview redactedInterview=tablesService.getRecordById(interview_id, Interview.class);
 		if(!redactedInterview.getStringStart_date().equals(updateStartDateString)
 				||!redactedInterview.getStringEnd_date().equals(updateEndDateString)){
@@ -49,6 +54,15 @@ public class HRController {
 			redactedInterview.setStringEnd_date(updateEndDateString);
 		}
 		tablesService.updateRecord(redactedInterview);
+		}
+		else{
+			map.put("erroMessage", "Конец собеседования не может быть раньше его начала или совпадать с ним.");
+		}
+	}
+	else{
+		map.put("erroMessage","Введённые данные не соответствуют шаблону.");
+	}
+
 		map.put("interviews",tablesService.getAllRecords(Interview.class));
 		return "hr";
 		}
@@ -59,8 +73,10 @@ public class HRController {
 	
 	
 	@RequestMapping(value="hr_create_interview")
-	public String createInterview(HttpServletRequest request, Map<String, Object> map){
+	public String createInterview(HttpServletRequest request, HttpSession session,
+			Map<String, Object> map){
 		
+		if(((Integer)session.getAttribute("user_type_id")).equals(2)){
 		if(!((String)request.getParameter("date")).isEmpty()
 				&&!((String)request.getParameter("startTime")).isEmpty()
 				&&!((String)request.getParameter("endTime")).isEmpty()){
@@ -71,23 +87,40 @@ public class HRController {
 		String newEndDateString=((String)request.getParameter("date"))
 				.concat(" "+(String)request.getParameter("endTime"));
 		
+		if(isInterviewDateStringValid(newStartDateString)
+				&&isInterviewDateStringValid(newEndDateString))
+		{
+			if(isInterviewStartEarlierThanEnd((String)request.getParameter("startTime"),
+					(String)request.getParameter("endTime"))){
 		Interview newInterview=new Interview();
 		newInterview.setStringEnd_date(newEndDateString);
 		newInterview.setStringStart_date(newStartDateString);
 		newInterview.setSeats(0);
 		tablesService.addRecord(newInterview);
 		}
+			else{
+				map.put("erroMessage", "Конец собеседования не может быть раньше его начала или совпадать с ним.");
+			}
+		}
+		else{
+			map.put("erroMessage","Введённые данные не соответствуют шаблону.");
+		}
+		}
 		
 		List<Interview> interviews=tablesService.getAllRecords(Interview.class);		
 		map.put("interviews",interviews);
 		return "hr";
+		}
+		else{
+			return "redirect:/index";
+		}
 	}
 	
 	@RequestMapping(value="hr_delete_interview_{interview_id}")
 	public String deleteInterviewFromHRPage(HttpServletRequest request, HttpSession session,
 			@PathVariable("interview_id") Integer interview_id,	Map<String, Object> map){
 	
-		if(((Integer)session.getAttribute("user_type")).equals(2)){
+		if(((Integer)session.getAttribute("user_type_id")).equals(2)){
 		tablesService.deleteRecord(tablesService.getRecordById(interview_id, Interview.class));
 		
 		map.put("interviews",tablesService.getAllRecords(Interview.class));
@@ -96,5 +129,21 @@ public class HRController {
 		else{
 			return "redirect:/index";
 		}
+	}
+	
+	private boolean isInterviewDateStringValid(String interviewDateStrig){
+		String pattern="201[3-9]-([0][1-9]|[1][0-2])-([0-2][0-9]|[3][0-1]) ([0-1][0-9]|2[0-4]):[0-5][0-9]";
+		return interviewDateStrig.matches(pattern);
+	}
+	
+	private boolean isInterviewStartEarlierThanEnd(String start, String end){
+		if(Integer.parseInt(start.substring(0,2))>Integer.parseInt(end.substring(0,2)))
+			return false;
+		if(Integer.parseInt(start.substring(0,2))<Integer.parseInt(end.substring(0,2)))
+			return true;
+		if(Integer.parseInt(start.substring(3))<Integer.parseInt(end.substring(3)))
+			return true;
+		else
+			return false;
 	}
 }
